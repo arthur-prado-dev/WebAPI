73b4b37cf269b6c4514511a36095e759a8592a98
Arthur <arthurhsprado@gmail.com>
2025-10-06 00:02:48 -0300

    Commit inicial e criação da entidade principal

---
+++ TodoApi/Models/Todo.cs
@@ -0,0 +1,29 @@
+﻿namespace TodoApi.Models;
+
+public class Todo
+{
+    private static int _id = 1;
+    
+    public int Id { get; }
+    public string? Title { get; private set; }
+    public bool? Done { get; private set; }
+
+    public Todo(string title, bool done)
+    {
+        Id = _id;
+        Title = title;
+        Done = done;
+
+        _id++;
+    }
+
+    public bool IsValid()
+    {
+        return Id > 0 && Title is not null && Done is not null;
+    }
+
+    public bool IsNotValid()
+    {
+        return !IsValid();
+    }
+}
